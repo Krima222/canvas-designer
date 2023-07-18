431f9e65ed3da02738a981c996a770d410acacea
Krima222 <manackaakristina@gmail.com>
2023-07-18 18:34:50 +0300

    fead: add functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,56 @@
+import { ElementType } from '../../types/ElementType';
+
 import classes from './Header.module.scss';
 
 import logo from '../../img/logo.svg';
+import arrowLeft from '../../img/arrow-left.svg';
+import arrowRight from '../../img/arrow-right.svg';
 
-export function Header() {
+type CustomElementType = ElementType | File | null;
+
+interface HeaderProps {
+  handleElementSelect: (element: CustomElementType) => void;
+  handleOpen: (element: boolean) => void;
+}
+
+export function Header({ handleElementSelect, handleOpen }: HeaderProps) {
+  const handleModalOpenButtonClick = () => {
+    handleOpen(true);
+  };
+
+  const handleCleareButtonClick = () => {
+    handleElementSelect('cleare');
+  };
+
+  const handleRedoButtonClick = () => {
+    handleElementSelect('redo');
+  };
+
   return (
     <div className={classes.header}>
       <img src={logo} alt="logo" />
       <div className={classes.header__wrapper}>
-        <button></button>
-        <button></button>
-        <button>Удалить всё</button>
-        <button>Сохранить</button>
+        <button className={classes.header__arrow}>
+          <img src={arrowLeft} alt="arrowLeft" />
+        </button>
+        <button
+          className={classes.header__arrow}
+          onClick={handleRedoButtonClick}
+        >
+          <img src={arrowRight} alt="arrowRight" />
+        </button>
+        <button
+          className={classes.header__button}
+          onClick={handleCleareButtonClick}
+        >
+          Удалить всё
+        </button>
+        <button
+          className={classes.header__button}
+          onClick={handleModalOpenButtonClick}
+        >
+          Сохранить
+        </button>
       </div>
     </div>
   );
